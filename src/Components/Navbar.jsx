1c03933a70382f6224fbfb42ff8000fff96ceeec
subrotto <subrotokumargunopu@gmail.com>
2025-01-22 02:39:04 +0600

    lastone

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -34,10 +34,10 @@ const routes=[
 
     return (
         <div style={{backgroundColor:'#1A1A1A',color:'#E5E6EB'}} className='p-6   bg-transparent relative z-10'>
-             <nav className='flex justify-around '>
+             <nav className='md:flex justify-around '>
           <Link href='/'><Logo></Logo></Link>
-          <ul>
-            {routes?.map(route=><Link className={`mr-8 `} href={route.path} key={route.path}> <button className='px-6 py-2 hover:border hover:border-white hover:border-2 hover:rounded-full  text-xs hover:text-white hover:bg-black'>{route.title}</button> </Link>)}
+          <ul className='mt-8 md:mt-0'>
+            {routes?.map(route=><Link className={`md:mr-8 `} href={route.path} key={route.path}> <button className='px-6 py-2 hover:border hover:border-white hover:border-2 hover:rounded-full  text-xs hover:text-white hover:bg-black'>{route.title}</button> </Link>)}
             
           </ul>
           
